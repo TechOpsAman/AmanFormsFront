ff621f143fb600fa0dec0d84e235ba4df339b8d1
danivngopro <danivngopro@gmail.com>
2022-11-30 10:41:35 +0200

    creating Homepage tsx file

--- forms/src/App.tsx
+++ forms/src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import SurveyCreationPage from "./pages/SurveyCreationPage/SurveyCreationPage";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
     <div className="survey-creation-name-container">
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/createSurvey" element={<SurveyCreationPage surveyName=''/>} />
       </Routes>
     </div>
