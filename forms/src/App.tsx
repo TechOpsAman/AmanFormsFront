8737e120ce1f19de7229246aa94123f2087e42c3
danivngopro <danivngopro@gmail.com>
2022-11-30 16:08:25 +0200

    init: init a navbar and first modify of it

--- forms/src/App.tsx
+++ forms/src/App.tsx
@@ -2,15 +2,22 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import SurveyCreationPage from "./pages/SurveyCreationPage/SurveyCreationPage";
 import HomePage from "./pages/HomePage/HomePage";
+import { Navbar } from "./components/form/Navbar";
 
 function App() {
   return (
-    <div className="survey-creation-name-container">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/createSurvey" element={<SurveyCreationPage surveyName=''/>} />
-      </Routes>
-    </div>
+    <>
+      <Navbar name='דניאל ונטורה' id='8599492' tNumber='T87475544'/>
+      <div className="survey-creation-name-container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/createSurvey"
+            element={<SurveyCreationPage surveyName="" />}
+          />
+        </Routes>
+      </div>
+    </>
   );
 }
 
