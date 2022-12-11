61a91337b846048194b8953adfd8af3b7706f6db
cr9169 <bar.gabay.home@gmail.com>
2022-12-11 17:09:36 +0200

    feat: config

--- forms/src/services/compositor.service.ts
+++ forms/src/services/compositor.service.ts
@@ -1,17 +1,19 @@
 import axios from "axios";
 import { ISurveyAnswers } from "../interfaces/answers/iSurvey";
 import { ISurveyQuestions } from "../interfaces/questions/iSurvey";
+import { config } from "../config";
 
 export default class CompositorService {
-  static api = "http://localhost:3003/api/compositor";
-
   static async getSurveyQuestionsAndUsersAnswers(
     surveyId: string
   ): Promise<(ISurveyQuestions | ISurveyAnswers[])[]> {
-    const surveyQuestionsAndUsersAnswers = await axios
-      .get(`${CompositorService.api}/getSurveyResults?surveyId=${surveyId}`)
-      .then((res) => res.data);
-
-    return surveyQuestionsAndUsersAnswers;
+    return axios
+      .get(
+        `${config.compositor.compositorConnectionString}/getSurveyResults?surveyId=${surveyId}`
+      )
+      .then((res) => res.data)
+      .catch((err) => {
+        if (err.response.status !== 404) console.log(err);
+      });
   }
 }
