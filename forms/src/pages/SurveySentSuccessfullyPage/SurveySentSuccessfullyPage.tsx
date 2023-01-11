6944a57da53b7af27cdb4116f1c9220e475df76d
cr9169 <bar.gabay.home@gmail.com>
2023-01-11 14:21:14 +0200

    feat: added SurveySentSuccessfullyPage trmplate

---
+++ forms/src/pages/SurveySentSuccessfullyPage/SurveySentSuccessfullyPage.tsx
@@ -0,0 +1,11 @@
+const SurveySentSuccessfullyPage = () => {
+  // keep working on it (should have massage and upper navbar)
+  return (
+    <div>
+      <h1>404 Error</h1>
+      <h2>Ca'nt find survey</h2>
+    </div>
+  );
+};
+
+export default SurveySentSuccessfullyPage;
